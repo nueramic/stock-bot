4e4c698d514696354225c8ad5d4826e9f122278c
vktrbr <viktorbarbarich@gmail.com>
2022-10-30 02:20:41 +0300

    Синхронная структура портфеля ценных бумаг. Реализованы структуры покупки на бирже, бумаги с атрибутами, истории портфеля и самого портфеля

---
+++ simulation/sim_macd_conservative.py
@@ -0,0 +1,5 @@
+from src.strategies.strategy_macd import get_decision_macd_conservative_strategy
+from src.parse_securities.async_moex import get_security_history
+
+
+
--- src/portfolio/base_portfolio.py
+++ src/portfolio/base_portfolio.py
@@ -1,10 +1,117 @@
 from collections import defaultdict
-from typing import Union, NamedTuple
+from copy import copy
+from typing import Union, Self
 
-SecurityState = NamedTuple('AmountPrice', amount=int, price=float)
+import pandas as pd
+from pandas import Timestamp
+
+PRICE_PRECISION = 16
 
 
-# TODO: Дописать функционал портфеля ценных бумаг
+class SecurityState:
+
+    def __init__(self, amount: int = 0, price: float = 0):
+        """
+        Конструктор класса, который будет хранить состояние конкретной бумаги.
+
+        :param amount: количество актива;
+        :param price: цена актива
+        """
+        self.amount: int = amount
+        self.price: float = round(price, PRICE_PRECISION)
+        self.history_sales: list[float] = [amount * self.price] if amount != 0 else []
+
+    def __repr__(self) -> str:
+        """
+        :return: Возвращает строку с информацией о бумаге в портфеле
+        """
+        return str({'amount': self.amount, 'price': self.price})
+
+    def security_value(self) -> float:
+        """
+        Возвращает стоимость всех бумаг: amount * price
+
+        :return: стоимость всех бумаг
+        """
+        return self.price * self.amount
+
+    def update_state(self, new_amount: int = 0, new_price: float = 0) -> None:
+        """
+        Обновляет состояние бумаги
+
+        :param new_amount: количество актива;
+        :param new_price: цена актива
+        """
+
+        self.history_sales.append(self.amount * self.price - new_amount * new_price)
+
+        self.amount = new_amount
+        self.price = round(new_price, PRICE_PRECISION)
+
+    def short_state(self) -> Self:
+        """
+        :return: возвращает состояние бумаги без ее истории. Не изменяет объект
+        """
+        copy_self = copy(self)
+        copy_self.history_sales = []
+        return copy_self
+
+
+class PortfolioHistory:
+
+    def __init__(self):
+        """
+        Конструктор класса, отвечающего за историю по портфелю
+        """
+
+        self.__history: dict[  # словарь с ключом - номером изменения и данными: дата и время, размер портфеля и состав
+            int, tuple[Timestamp, float, tuple[tuple[str, SecurityState]]]
+        ] = dict()
+        self.__counter = 0
+
+    def log_history(self,
+                    balance: [float, int],
+                    timestamp: Timestamp,
+                    securities: tuple[str],
+                    sec_states: tuple[SecurityState]):
+        """
+        Логирование состояния портфеля
+
+        :param balance: новый баланс портфеля
+        :param timestamp: дата и    время изменения
+        :param securities: название бумаги
+        :param sec_states: количество и цена бумаги в портфеле
+        :return: только обновляет историю
+        """
+
+        self.__counter += 1
+
+        sec_states: list[SecurityState] = list(map(lambda x: x.short_state(), sec_states))
+        self.__history[self.__counter] = (timestamp, balance, tuple(zip(securities, sec_states)))
+
+    def __repr__(self) -> str:
+        return str(self.__history)
+
+    def get_history(self):
+        return self.__history
+
+
+class StockPurchase:
+
+    def __init__(self, name: str, amount: int, price: float, exchange_fees: float = 0):
+        """
+        Конструктор класса покупки на бирже нового актива.
+
+        :param name: название бумаги. Оно обязательно будет приведено к верхнему регистру;
+        :param amount: количество купленных бумаг. Может быть отрицательным при продаже;
+        :param price: цена продажи / покупки
+        :param exchange_fees: комиссии по операции
+        """
+        self.name = name.upper()
+        self.amount = int(amount)
+        self.price = price
+        self.exchange_fees = exchange_fees
+
 
 class Portfolio:
 
@@ -18,41 +125,145 @@ class Portfolio:
             raise ValueError('Баланс должен быть числом типа int или float')
 
         self.balance = balance
+        self.balance_history = list[defaultdict()]
         self.securities = defaultdict(SecurityState)  # инициализируем пустой дефолтный словарь - портфель бумаг
+        self.history = PortfolioHistory()
 
-    def update_securities(self, *args: tuple[str, int, float]) -> None:
+    def update_securities(self, *args: StockPurchase) -> None:
         """
         Обновляет бумаги в портфеле.
 
-        :param args: Кортежи (тикер, количество, цена). Если позиция короткая, то количество отрицательное.
+        :param args: Кортежи (тикер, SecurityState). Если позиция короткая, то количество отрицательное.
                      Если бумаги были проданы или куплены количество соответственно положительное или отрицательное.
                      После обновления цена будет перевзвешена в соответствии с количеством. Если мы стоим в короткой
                      позиции, то покупка просто сократит количество бумаг, а на баланс может поступить положительная
                      разница, при ее наличии. Если в короткой позиции мы продаем бумаги, то их цена перевзвешивается;
         :return: обновляет внутренний портфель
         """
-        for val in args:
-            pass
+        for security in args:
+            security: StockPurchase
+            self._update_value_securities(security.name,
+                                          SecurityState(security.amount, security.price),
+                                          security.exchange_fees)
+
+        self.hook()
 
     def hook(self):
-        pass
+        sec_names = []
+        sec_states = []
+        for name, state in self.securities.items():
+            sec_names.append(name)
+            sec_states.append(state)
+
+        sec_names: list[str]
+        self.history.log_history(self.balance, pd.Timestamp.now(), tuple(sec_names), tuple(sec_states))
 
     def _update_value_securities(self,
-                                 cur_sec_state: SecurityState,
-                                 bought_sec_state: SecurityState) -> SecurityState:
+                                 security: str,
+                                 update_security_state: SecurityState,
+                                 exchange_fees: float = 0) -> None:
+
         """
         Обновляет количество бумаг в портфеле и считает финансовый эффект, потом обновляет баланс
 
-        :param cur_amount: текущее количество бумаг и их стоимость;
-        :param new_amount: купленное / проданное количество бумаг и их стоимость;
-        :return: новое количество бумаг в портфеле, их стоимость
+        :param security: имя бумаги;
+        :param update_security_state: купленное / проданное количество бумаг и их стоимость;
+        :param exchange_fees: комиссия за сделку с этой бумагой;
+        :return: обновляет состояние портфеля и ничего не возвращает
         """
 
-        if cur_sec_state.amount >= 0 and bought_sec_state.amount >= 0:
-            new_amount = cur_sec_state.amount + bought_sec_state.amount
-            new_value = cur_sec_state.amount * cur_sec_state.price + bought_sec_state.amount * bought_sec_state.price
-            new_price = new_value / new_amount
+        cur_state: SecurityState = self.securities[security]
 
-        elif cur_sec_state.amount >= 0 and bought_sec_state.amount < 0:
-            pass
-        return SecurityState(new_amount, new_price)
+        # Если мы докупаем бумаги, то их количество увеличивается, и цена взвешивается. Баланс просто уменьшается
+
+        # Мы без бумаг или в длинной позиции
+        if cur_state.amount >= 0:
+
+            # Покупаем бумаги
+            if update_security_state.amount >= 0:
+                self._same_directional_update(security, update_security_state)
+
+            # Продаем бумаги
+            else:
+                self._different_directional_update(security, update_security_state)
+
+        # Короткая позиция уже открыта
+        else:
+            # Покупаем бумаги
+            if update_security_state.amount >= 0:
+                self._different_directional_update(security, update_security_state)
+
+            # Продаем бумаги
+            else:
+                self._same_directional_update(security, update_security_state)
+
+        self.balance -= exchange_fees
+
+    def _same_directional_update(self,
+                                 security: str,
+                                 update_security_state: SecurityState) -> None:
+        """
+        Покупка бумаг в длинной (или без бумаг) или продажа в короткой. То есть количество имеющихся бумаг сейчас
+        того же знака, что текущая операция по бумагам.
+
+        :param security: имя бумаги;
+        :param update_security_state: купленное количество бумаг и их стоимость;
+        :return: обновляет состояние портфеля и ничего не возвращает
+        """
+        cur_state: SecurityState = self.securities[security]
+
+        # обновляем количество
+        new_amount = cur_state.amount + update_security_state.amount
+
+        # обновляем цену бумаги в портфеле
+        new_price = cur_state.security_value() + update_security_state.security_value()
+        new_price /= cur_state.amount + update_security_state.amount
+
+        # обновляем баланс и состояние по бумаге
+        self.balance -= update_security_state.security_value()
+        self.securities[security].update_state(new_amount, new_price)
+
+    def _different_directional_update(self,
+                                      security: str,
+                                      update_security_state: SecurityState) -> None:
+        """
+        Продажа бумаг в длинной позиции или покупка в короткой.
+
+        :param security: имя бумаги;
+        :param update_security_state: проданное количество бумаг и их стоимость;
+        :return: обновляет состояние портфеля и ничего не возвращает
+        """
+        cur_state: SecurityState = self.securities[security]
+
+        # обновляем количество, если продали или купили не больше текущей позиции
+        if abs(cur_state.amount) >= abs(update_security_state.amount):
+            new_amount = cur_state.amount + update_security_state.amount
+            new_price = cur_state.price
+
+        # все продали / купили и вышли в другую позицию
+        else:
+            new_amount = cur_state.amount + update_security_state.amount
+            new_price = update_security_state.price
+
+        # обновляем баланс
+        self.balance -= update_security_state.security_value()
+        self.securities[security].update_state(new_amount, new_price)
+
+
+if __name__ == '__main__':
+    port = Portfolio()
+    print(port.securities, port.balance)
+
+    port.update_securities(StockPurchase('SBER', 100, 120))
+    print(port.securities, port.balance)
+
+    port.update_securities(StockPurchase('SBER', 200, 110), StockPurchase('appl', 200, 200))
+    print(port.securities, port.balance)
+
+    port.update_securities(StockPurchase('SBER', -300, 140), StockPurchase('appl', -200, 190))
+    print(port.securities, port.balance)
+
+    print(port.securities['SBER'].history_sales)
+    print(sum(port.securities['SBER'].history_sales))
+    for epoch in port.history.get_history().items():
+        print(epoch)
